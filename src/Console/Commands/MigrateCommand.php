807c446bd1d6d1e459114e28df2f61f879cc7e6c
Abdelrahman Omran <me@omranic.com>
2018-10-05 11:08:52 +0400

    Rename package to rinvex/laravel-tags and bump versions

--- src/Console/Commands/MigrateCommand.php
+++ src/Console/Commands/MigrateCommand.php
@@ -30,6 +30,6 @@ class MigrateCommand extends Command
     public function handle(): void
     {
         $this->warn($this->description);
-        $this->call('migrate', ['--step' => true, '--path' => 'vendor/rinvex/tags/database/migrations', '--force' => $this->option('force')]);
+        $this->call('migrate', ['--step' => true, '--path' => 'vendor/rinvex/laravel-tags/database/migrations', '--force' => $this->option('force')]);
     }
 }
